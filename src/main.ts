a97f0cc1d9035f4e770eed920897c5b91d73a153
Kayla <kaylatran2021@gmail.com>
2025-07-09 18:49:51 -0700

    feat: update agent to fill and submit medical form

--- src/main.ts
+++ src/main.ts
@@ -1,16 +1,9 @@
-import { generateText } from "ai";
-import { model } from "./_internal/setup";
+import { fillForm } from "./_internal/fill";
+import { submitForm } from "./_internal/submit";
 import { createSession } from "./session";
 
 export async function main() {
-  // This will automatically create a chromium instance, connect, and navigate to the given url.
-  // You are given a playwright page back.
-  const page = await createSession("https://www.google.com");
-
-  // We've given you an model (gemini-2.5-flash-preview-04-17), you can use the vercel AI SDK to generate text, setup tools, etc.
-  // Ensure you have set the GOOGLE_GENERATIVE_AI_API_KEY environment variable.
-  const response = await generateText({
-    model,
-    prompt: "How many r's are in strawberry?",
-  });
+  const page = await createSession("https://magical-medical-form.netlify.app/");
+  await fillForm(page);
+  await submitForm(page);
 }

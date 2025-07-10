3b0ec9b910aca6eb64f6806c383462b662e710a6
Kayla <kaylatran2021@gmail.com>
2025-07-10 09:23:37 -0700

    chore: define interface for medical form fields

---
+++ src/types/formData.ts
@@ -0,0 +1,12 @@
+export interface FormData {
+  firstName: string;
+  lastName: string;
+  dateOfBirth: string;
+  medicalId: string;
+  gender: string;
+  bloodType: string;
+  allergies: string;
+  medications: string;
+  emergencyContactName: string;
+  emergencyContactPhone: string;
+}

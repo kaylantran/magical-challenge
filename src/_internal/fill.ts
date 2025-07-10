b2d8f5c6466dc6b4ee5dc97611dc06ae9da510e1
Kayla <kaylatran2021@gmail.com>
2025-07-09 19:13:07 -0700

    feat: add helper to open and fill medical section of form

--- src/_internal/fill.ts
+++ src/_internal/fill.ts
@@ -1,8 +1,16 @@
 import { Page } from "playwright";
 
-export async function fillForm(page:Page) {
+export async function fillPersonal(page: Page) {
   await page.fill("input[name=firstName]", "John");
   await page.fill("input[name=lastName]", "Doe");
   await page.fill("input[name=dateOfBirth]", "1990-01-01");
   await page.fill("input[name=medicalId]", "91927885");
+}
+
+export async function fillMedical(page: Page) {
+  await page.getByRole("button", { name: "Medical Information" }).click();
+  await page.selectOption('select[name="gender"]', { label: "Male" });
+  await page.selectOption('select[name="bloodType"]', { label: "O+" });
+  await page.fill('textarea[name="allergies"]', "None");
+  await page.fill('textarea[name="medications"]', "Ibuprofen");
 }

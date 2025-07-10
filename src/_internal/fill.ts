2c57471a9aa817293c037e0d8cfd67bc818df03a
Kayla <kaylatran2021@gmail.com>
2025-07-09 18:16:04 -0700

    feat: add helper to populate form fields

---
+++ src/_internal/fill.ts
@@ -0,0 +1,8 @@
+import { Page } from "playwright";
+
+export async function fillForm(page:Page) {
+  await page.fill('input[name=firstName', "John")
+  await page.fill('input[name=lastName', "Doe")
+  await page.fill('input[name=dob', "1990-01-01")
+  await page.fill('input[name=medicalId', "91927885")
+}
